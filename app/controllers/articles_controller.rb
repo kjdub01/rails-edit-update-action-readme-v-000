18f6d48703c0d145ca7159ec254e5a4692e71050
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-11 02:32:55 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,13 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
-
+  
   # add edit and update methods here
+  
+  def edit
+  end
+  
+  def update
+  end
+  
 end

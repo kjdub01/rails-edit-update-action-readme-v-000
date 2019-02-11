bdf5f54d6940594d7cb10fa7b876341a2b34ccca
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-11 03:01:46 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,11 +24,11 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
-  
+
   def update
     @article = Article.find(params[:id])
-  @article.update(title: params[:title], description: params[:description])
-  redirect_to article_path(@article)
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
   end
   
 end
